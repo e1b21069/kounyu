016848ce55969e6105c4305aa7a53a9f7d952a25
e1b21030 <e1b21030@st.oit.ac.jp>
2023-10-24 15:18:37 +0900

    果物のレコード表示

--- src/main/java/oit/is/lec05/team6/kounyu/controller/Sample51Controller.java
+++ src/main/java/oit/is/lec05/team6/kounyu/controller/Sample51Controller.java
@@ -1,14 +1,31 @@
 package oit.is.lec05.team6.kounyu.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import oit.is.lec05.team6.kounyu.model.Fruit;
+import oit.is.lec05.team6.kounyu.model.FruitMapper;
 
 @Controller
 @RequestMapping("/sample5")
 public class Sample51Controller {
-    @GetMapping("/step1")
-    public String Saple51(){
-        return "sample51.html";
-    }
+
+  @Autowired
+  FruitMapper fMapper;
+
+  @GetMapping("/step1")
+  public String Saple51() {
+    return "sample51.html";
+  }
+
+  @GetMapping("step2")
+  public String sample52(ModelMap model) {
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
 }
