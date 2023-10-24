43d263a7a48f921fc4986f18d7eb09ad4e1e46e2
e1b21069 <e1b21069@oit.ac.jp>
2023-10-24 14:01:51 +0900

    初期化を行う

---
+++ src/main/java/oit/is/lec05/team6/kounyu/KounyuApplication.java
@@ -0,0 +1,13 @@
+package oit.is.lec05.team6.kounyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KounyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KounyuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec05/team6/kounyu/KounyuApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec05.team6.kounyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KounyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
